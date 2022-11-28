ba9c7f655bc14a8837a7d7030e38baacc6000bea
Harrison Hassig <harrisonhassig@gmail.com>
2022-11-28 21:10:46 +0100

    Ready to seed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_170659) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_194003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_170659) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,6 @@ require "open-uri"
 
 Faker::Config.locale = "ca-CAT"
 
-# now = Time.now
-# ninety_day_ago = now - (60 * 60 * 24 * 90)
-
 venue_photos = []
 csv_text = File.read(Rails.root.join('lib','seeds','cafe_photos.csv'))
 csv = CSV.parse(csv_text, encoding: 'UTF-8')
@@ -20,47 +17,47 @@ csv.each do |row|
   user_photos << row
 end
 
-# x = 0
-# while x < user_photos.size - 20
-#   name = Faker::Name.name
-#   puts name
-#   user = User.new(
-#     is_owner: false,
-#     name: name,
-#     username: name.strip,
-#     email: Faker::Internet.email,
-#     password: Faker::Blockchain::Bitcoin.address,
-#   )
-#   file = URI.open(user_photos[x][0])
-#   user.photo.attach(io: file, content_type: "image/png", filename: "user#{user.id}.jpg")
-#   user.save!
-#   puts "User #{x} saved!"
-#   x += 1
-# end
+x = 0
+while x < user_photos.size - 20
+  name = Faker::Name.name
+  puts name
+  user = User.new(
+    is_owner: false,
+    name: name,
+    username: name.strip,
+    email: Faker::Internet.email,
+    password: Faker::Blockchain::Bitcoin.address,
+  )
+  file = URI.open(user_photos[x][0])
+  user.photo.attach(io: file, content_type: "image/png", filename: "user#{user.id}.jpg")
+  user.save!
+  puts "User #{x} saved!"
+  x += 1
+end
 
-# x = 0
-# while x < 20
-#   name = Faker::Name.name
-#   user = User.new(
-#     is_owner: true,
-#     name: name,
-#     username: name.strip,
-#     email: Faker::Internet.email,
-#     password: Faker::Blockchain::Bitcoin.address,
-#   )
-#   file = URI.open(user_photos[x + 279][0])
-#   user.photo.attach(io: file, content_type: "image/png", filename: "user#{user.id}.jpg")
-#   user.save!
-#   puts "User #{x + 279} saved!"
-#   x += 1
-# end
+x = 0
+while x < 20
+  name = Faker::Name.name
+  user = User.new(
+    is_owner: true,
+    name: name,
+    username: name.strip,
+    email: Faker::Internet.email,
+    password: Faker::Blockchain::Bitcoin.address,
+  )
+  file = URI.open(user_photos[x + 279][0])
+  user.photo.attach(io: file, content_type: "image/png", filename: "user#{user.id}.jpg")
+  user.save!
+  puts "User #{x + 279} saved!"
+  x += 1
+end
 
 x = 0
 while x < 100
   venue = Venue.new(
     name: Faker::Coffee.blend_name,
     address: Faker::Address.full_address,
-    website: Faker::Blockchain::Bitcoin.address,
+    website: "#{Faker::Blockchain::Bitcoin.address.com}",
     category: ["Bar", "Cafe", "Library", "Cafe", "Cafe", "Cafe", "Cafe", "Cafe", "Cafe", "Cafe", "Cafe"].sample,
     user: User.all.sample,
     power_outlets: [true, false].sample,
@@ -76,27 +73,42 @@ while x < 100
   x += 1
 end
 
-# OFFERS ?
-# x = 0
-# while x < 100
-#   Offer.create(
-#     venue: x,
-#     offer: [5.0, 10.0, 15.0, 20.0, 30.0].sample
-#   )
-#   x += 8
-# end
+x = 0
+while x < 100
+  venue_offer = VenueOffer.create(
+    venue: Venue.all.sample,
+    coupon: [5.0, 10.0, 15.0, 20.0, 30.0].sample
+  )
+  venue_offer.save!
+  x += 8
+end
 
-# x = 0
-# while x < 300
-#   Review.create(
-#     calls: [true, true, true, true, true, true, true, false, false, false].sample,
-#     cafe_con_leche_price: rand(1.4..2.3),
-#     wifi: [rand(30..300), rand(30..150)],
-#     power_outlets: [true, true, true, true, true, true, true, false, false, false].sample,
-#     natural_light: [true, true, true, true, true, true, true, false, false, false].sample,
-#     # timestamp: rand(ninety_day_ago..now),
-#     stars: rand(0..5),
-#     food_price: rand(0..5)
-#   )
-#   x += 1
-# end
+x = 101
+while x < 171
+  booking = Booking.new(
+    user: User.all.sample,
+    venue: Venue.all.sample,
+    date: Date.today - rand(150)
+  )
+  booking.save!
+  x += 1
+end
+
+x = 0
+while x < 70
+  review = Review.new(
+    suited_for_calls: [true, true, true, true, true, true, true, false, false, false].sample,
+    coffe_price: rand(0..5),
+    wifi_down: rand(30..300),
+    wifi_up: rand(30..150),
+    power_outlets: [true, true, true, true, true, true, true, false, false, false].sample,
+    natural_light: [true, true, true, true, true, true, true, false, false, false].sample,
+    rating: rand(0..5),
+    food_price: rand(0..5),
+    comment: Faker::Restaurant.review,
+    title: Faker::Coffee.notes,
+    booking: Booking.find_by_id(x)
+  )
+  review.save!
+  x += 1
+end
