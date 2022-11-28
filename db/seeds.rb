c84c5a8e831f068c9a78197b22f89abc5351b917
Harrison Hassig <harrisonhassig@gmail.com>
2022-11-28 14:29:33 +0100

    Seed file save

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 require 'faker'
 Faker::Config.locale = 'ca-CAT'
 
 now = Time.now
 ninety_day_ago = now - (60 * 60 * 24 * 90)
 
-300.times do
+100.times do
   Cafe.create(
     name: Faker::Coffee.blend_name,
     Faker::Address.full_address,
     website: Faker::Blockchain::Bitcoin.address,
-    photo:
+    photo: ,
   )
-
+end
+# OFFERS ?
+300.times do
   Review.create(
     calls: [true, true, true, true, true, true, true, false, false, false].sample,
     cafe_con_leche_price: rand(1.4..2.3),
-    wifi_up: rand(30..150),
-    wifi_down: rand(30..300),
+    wifi: [rand(30..300), rand(30..150)],
     power_outlets: [true, true, true, true, true, true, true, false, false, false].sample,
     natural_light: [true, true, true, true, true, true, true, false, false, false].sample,
     timestamp: rand(ninety_day_ago..now),
     stars: rand(0..5),
     food_price: ["cheap", "normal", "expensive"].sample
+  )
+end
+200.times do
+  User.create(
+
   )
 end
