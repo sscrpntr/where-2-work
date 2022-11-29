16d5576787ffc250cd6ce6860ed576ef4a6cd3b8
Harrison Hassig <harrisonhassig@gmail.com>
2022-11-29 12:52:23 +0100

    Display average rating and opening hours

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -9,7 +9,6 @@ class VenuesController < ApplicationController
     # @markers = [{ lat: @laptop.latitude, lng: @laptop.longitude }]
     @booking = Booking.new
     @venues = Venue.all
-    # @average_review = average_reviews(@venue)
   end
 
   private
@@ -18,12 +17,4 @@ class VenuesController < ApplicationController
     params.require(:venue).permit(:user, :category, :name, :address, :website, :power_outlets, :natural_light, :suited_for_calls, :opening_time, :closing_time)
   end
 
-  # def average_reviews(venue)
-  #   reviews = Review.where(venue:)
-  #   sum = 0
-  #   reviews.each do |review|
-  #     sum += review.rating
-  #   end
-  #   return (sum.to_f / reviews.count).round(2)
-  # end
 end
