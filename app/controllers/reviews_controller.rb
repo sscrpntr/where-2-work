d41a2425244ee1c1d8d1171d42008fe2f44f981d
Sergi Escarpenter <sergiescarpenter@gmail.com>
2022-11-29 15:34:28 +0100

    Reviews create/show/view made, user can do reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    if review.save
+      redirect_to venues_path(@venue)
+    else
+      render "venues/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :suited_for_calls, :wifi_down, :wifi_up, :power_outlets, :naural_light, :rating, :coffe_price, :food_price, :comment, :title)
+  end
 end
