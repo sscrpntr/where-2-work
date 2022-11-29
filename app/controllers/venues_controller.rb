9be7453bc9299488265cdb3ef7ecbae79fb0c42f
Sergi Escarpenter <sergiescarpenter@gmail.com>
2022-11-29 16:09:52 +0100

    Substitutes last push to avoid venue merging conflicts

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -10,22 +10,12 @@ class VenuesController < ApplicationController
     @booking = Booking.new
     @venues = Venue.all
     # @average_review = average_reviews(@venue)
-    @review = all_reviews(@venue)
   end
 
   private
 
   def venue_params
     params.require(:venue).permit(:user, :category, :name, :address, :website, :power_outlets, :natural_light, :suited_for_calls, :opening_time, :closing_time)
-  end
-
-  def all_reviews(venue)
-    reviews = Review.where(venue:)
-    sum = 0
-    reviews.each do |review_object|
-      sum += review_object.review
-    end
-    return (sum.to_f / reviews.count).round(2)
   end
 
   # def average_reviews(venue)
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,4 +4,36 @@ class Venue < ApplicationRecord
   has_many :venue_offers
   has_many :bookings
   has_many :reviews, through: :bookings
+
+  def all_reviews
+    reviews.all
+  end
+
+  def average_rating
+    sum = 0
+    reviews.each do |review|
+      sum += review.rating
+    end
+    return (sum.to_f / reviews.count).round(2)
+  end
+
+  def opening_time_display
+    if opening_time % 1 > 0
+      if opening_time < 10
+        return "0#{(opening_time - 0.5).to_i}:00"
+      else
+        return "#{(opening_time - 0.5).to_i}:00"
+      end
+    else
+      return "#{opening_time.to_i}:30"
+    end
+  end
+
+  def closing_time_display
+    if opening_time % 1 > 0
+      return "#{(closing_time - 5).to_i}:30"
+    else
+      return "#{closing_time.to_i}:30"
+    end
+  end
 end
