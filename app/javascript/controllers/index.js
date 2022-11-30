165dd0024b9ea1ff3442fcc588588d87ae27499d
Harrison Hassig <harrisonhassig@gmail.com>
2022-11-30 22:27:16 +0100

    Bye bye star rating

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -12,6 +12,3 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
-
-import StarRatingController from "./star_rating_controller"
-application.register("star-rating", StarRatingController)
