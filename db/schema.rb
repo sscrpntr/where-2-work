8878f70586a19eca24f1a1b69c5bfbf483f37fdb
Harrison Hassig <harrisonhassig@gmail.com>
2022-11-30 16:23:01 +0100

    Rand rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_163102) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_143418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_163102) do
     t.bigint "venue_id", null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "venue_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+    t.index ["venue_id"], name: "index_favorites_on_venue_id"
   end
 
   create_table "review_likes", force: :cascade do |t|
@@ -124,6 +133,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_163102) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "users"
   add_foreign_key "bookings", "venues"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "favorites", "venues"
   add_foreign_key "review_likes", "reviews"
   add_foreign_key "review_likes", "users"
   add_foreign_key "reviews", "bookings"
