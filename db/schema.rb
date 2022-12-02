4e330185e3ce77aefb9b7d9ef6d1d935b74dcea8
Harrison Hassig <harrisonhassig@gmail.com>
2022-12-02 15:18:00 +0100

    Bookings styled

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_143418) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_141529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,6 +104,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_143418) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "venue_id", null: false
+    t.date "visit_date"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
   end
