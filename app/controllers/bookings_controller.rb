93144e77b33f7efbb15f5856a3dbee1fb2438ea9
Sergi Escarpenter <sergiescarpenter@gmail.com>
2022-12-05 17:45:29 +0100

    Now you can edit bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -29,6 +29,20 @@ class BookingsController < ApplicationController
     redirect_to booking_path(@booking)
   end
 
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
