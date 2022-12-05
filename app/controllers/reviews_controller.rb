5979d6a0903e5c02b4099c064b069bf9fc2ed736
Harrison Hassig <harrisonhassig@gmail.com>
2022-12-05 15:26:33 +0100

    Single review view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,6 +15,8 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
     @booking_id = @review.booking_id
     @booking = Booking.find(@booking_id)
+    @user_id = @booking.user_id
+    @user = User.find(@user_id)
     @venue_id = @booking.venue_id
     @venue = Venue.find(@venue_id)
   end
