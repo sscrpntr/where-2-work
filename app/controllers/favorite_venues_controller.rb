87768c5a8e47e50bc13eb02fac9a98682c677551
Dilmer Alvarado <dilmer.alvarado@gmail.com>
2022-12-05 17:59:56 +0100

    favorite index

--- app/controllers/favorite_venues_controller.rb
+++ app/controllers/favorite_venues_controller.rb
@@ -1,19 +1,27 @@
 class FavoriteVenuesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show]
 
-  def create
-    @favorite_venue = FavoriteVenue.create(venue_id: params[:venue_id], user: current_user)
-    redirect_to venues_path
-  end
+  # def create
+  #   @favorite_venue = FavoriteVenue.create(venue_id: params[:venue_id], user: current_user)
+  #   redirect_to venues_path
+  # end
 
   def index
     @favorites = current_user.favorite_venues
   end
 
+  def new
+    @favorites = FavoriteVenue.new
+  end
 
-  private
-
-  def venue_params
-    params.require(:favorite_venue).permit(:venue_id, user_id)
+  def create
+    @favorites = FavoriteVenue.new(venue_id: params[:venue_id], user: current_user)
+    if @favorites.save!
+      redirect_to venues_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
+
+# <%= link_to "btn", user_favorite_venues_path(current_user,venue_id: venue.id), data: {turbo_method: :post} %>
