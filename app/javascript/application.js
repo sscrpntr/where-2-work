70ab3ba39091eb2df46353a01dafd2991a1d7f84
Sergi Escarpenter <sergiescarpenter@gmail.com>
2022-12-06 10:57:48 +0100

    Changes saved on application.js that were unsaved

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,25 +2,3 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 import "bootstrap";
-
-
-console.log('dont let me down pls');
-document.getElementById("create-booking").addEventListener("click", (event) => {
-  checkDateValue();
-});
-
-document.getElementById("booking_date").addEventListener("change", (event) => {
-  if (document.getElementById("booking_date") == null) {
-    document.getElementById("create-booking").disabled = true
-  } else {
-    document.getElementById("create-booking").disabled = false
-  }
-});
-
-
-
-const checkDateValue = () => {
-  if (document.getElementById("booking_date") == null) {
-    alert("You need to pick a date!");
-  }
-}
