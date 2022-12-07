a52dc56d15f565e081ecd0ddef52b3708daa29cb
Harrison Hassig <harrisonhassig@gmail.com>
2022-12-07 09:08:20 +0100

    Link manifest.json

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,9 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
+// app/assets/config/manifest.js
+// ...
+//= link manifest.json
 {
   "name": "App",
   "icons": [
