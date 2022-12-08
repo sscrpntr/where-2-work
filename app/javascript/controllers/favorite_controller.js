539ca60555b8e315b693958c8e44388196ea66c3
Harrison Hassig <harrisonhassig@gmail.com>
2022-12-08 15:48:02 +0100

    Favorite works

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -45,4 +45,42 @@ export default class extends Controller {
 
   }
 
+  removeFavoriteVenueShow(event) {
+
+
+    event.preventDefault()
+    const url = `/venues/${this.venueIdValue}/favorite_venues/${this.favoriteIdValue}`
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+          "Accept": "application/json",
+          "X-CSRF-Token": document.head.querySelector("[name='csrf-token']").content
+        }
+      })
+      .then(response => response.json())
+      .then((data) => {
+        this.element.innerHTML= `<a data-turbo-method="post" href="/venues/${this.venueIdValue}/favorite_venues"><i class="fa-regular fa-heart fav-background-show" data-action="click->favorite#drefresh"></i></a>`
+      })
+
+  }
+
+  addFavoriteVenueShow(event) {
+
+
+    event.preventDefault()
+    const url = `/venues/${this.venueIdValue}/favorite_venues`
+    fetch(url, {
+      method: "POST",
+      headers: {
+          "Accept": "application/json",
+          "X-CSRF-Token": document.head.querySelector("[name='csrf-token']").content
+        }
+      })
+      .then(response => response.json())
+      .then((data) => {
+        this.element.innerHTML= `<a data-turbo-method="delete" href="/venues/${this.venueIdValue}/favorite_venues"><i class="fa-solid fa-heart fav-background-show" data-action="click->favorite#refresh"></i></a>`
+      })
+
+  }
+
 }
